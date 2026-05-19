cf64a2fd2198c7dcc90a789cf37dbbe0ee833962
joshualotha <iamjoshualotha@gmail.com>
2026-05-19 22:11:11 +0300

    Load OwlCarousel and main.min.js synchronously (guarantees jQuery plugin order)

--- resources/views/partials/scripts.blade.php
+++ resources/views/partials/scripts.blade.php
@@ -1,15 +1,15 @@
-<!-- JavaScript Libraries (all deferred to eliminate render-blocking) -->
+<!-- JavaScript Libraries -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+<script src="{{ asset('lib/owlcarousel/owl.carousel.min.js') }}"></script>
 <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
 <script defer src="{{ asset('lib/wow/wow.min.js') }}"></script>
 <script defer src="{{ asset('lib/easing/easing.min.js') }}"></script>
 <script defer src="{{ asset('lib/waypoints/waypoints.min.js') }}"></script>
 <script defer src="{{ asset('lib/counterup/counterup.min.js') }}"></script>
 <script defer src="{{ asset('lib/lightbox/js/lightbox.min.js') }}"></script>
-<script defer src="{{ asset('lib/owlcarousel/owl.carousel.min.js') }}"></script>
 
 <!-- Template Javascript (minified for production) -->
-<script defer src="{{ asset('js/main.min.js') }}"></script>
+<script src="{{ asset('js/main.min.js') }}"></script>
 <script>
     // Defer non-critical initializations to AFTER LCP to avoid long main-thread tasks
     // that delay Largest Contentful Paint.
